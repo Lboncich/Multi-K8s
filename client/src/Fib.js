d9f79dbe21f1d2985e2a037ac99caa8be7f4eec5
Luke Boncich <Lboncich@gmail.com>
2020-09-21 15:05:47 -0700

    Changed Frontend text and reload on submit

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -61,7 +61,7 @@ class Fib extends Component {
             value={this.state.index}
             onChange={event => this.setState({ index: event.target.value })}
           />
-          <button>Submit</button>
+          <button onClick="window.location.reload();">Submit</button>
         </form>
 
         <h3>Indexes I have seen:</h3>
